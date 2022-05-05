d6c17565864dd6873059ce38fa8c242e0f4ea5c7
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-05 10:24:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xeou1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const collection = client.db("bike-warehouse").collection("inventories");
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = collection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
+  } finally {
+    //  await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   //   client.close();
+//   console.log("connected to db");
+// });
+
+app.get("/", (req, res) => {
+  res.send("running warehouse server ");
+});
+
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
